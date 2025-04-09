dea3d667cbaff55a87adf5935ef1062364c858dd
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-04-09 20:40:13 +0200

    Fix formatting and comments in SQL script for clarity

--- AlejandroGomez.sql
+++ AlejandroGomez.sql
@@ -1,11 +1,12 @@
---CONSULTAS
+-- CONSULTAS
 -- 1. Mostrar todos los ejemplares nuevos en venta de un libro según su ISBN.
 SELECT l.ISBN, l.TITULO, e.ID as ID_EJEMPLAR, e.ESTADO, e.FORMATO, e.PRECIO_VENTA FROM LIBRO_TABl, TABKE(l.EJEMPLARESVENTA) e WHERE l.ISBN = '0123456789123' AND e.ESTADO = 'Nuevo';
 
---2. Media del precio de venta por estado del ejemplar.
+-- 2. Media del precio de venta por estado del ejemplar.
 SELECT e.ESTADO, AVG(e.PRECIO_VENTA) as MEDIA_PRECIO FROM LIBRO_TAB l, TABLE(l.EJEMPLARESVENTA) e GROUP BY e.ESTADO;
 
---Trigger
+-- Trigger
+-- Trigger que impide la inserción de un alquiler si el usuario ya tiene 3 alquileres activos.
 CREATE OR REPLACE TRIGGER TGR_LIMITE_ALQUILER
 BEFORE INSERT ON ALQUILER_TAB
 FOR EACH ROW
