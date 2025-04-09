13fc3aa73a5e2f5b2d4e7e50b432db7fe80d2041
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-04-09 20:38:23 +0200

    Add Alejandro Gomez's trigger to limit active rentals per user to three

--- AlejandroGomez.sql
+++ AlejandroGomez.sql
@@ -5,3 +5,18 @@ SELECT l.ISBN, l.TITULO, e.ID as ID_EJEMPLAR, e.ESTADO, e.FORMATO, e.PRECIO_VENT
 --2. Media del precio de venta por estado del ejemplar.
 SELECT e.ESTADO, AVG(e.PRECIO_VENTA) as MEDIA_PRECIO FROM LIBRO_TAB l, TABLE(l.EJEMPLARESVENTA) e GROUP BY e.ESTADO;
 
+--Trigger
+CREATE OR REPLACE TRIGGER TGR_LIMITE_ALQUILER
+BEFORE INSERT ON ALQUILER_TAB
+FOR EACH ROW
+DECLARE
+    v_num_alquileres NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_num_alquileres FROM ALQUILER_TAB a, TABLE(a.LINEA_ALQUILER) l
+    WHERE DEREF(a.USUARIO).DNI = DEREF(:NEW.USUARIO).DNI AND a.FECHAINICIO + DEREF(l.EJEMPLAR_ALQUILER).DURACION_ALQUILER > SYSDATE;  --Se compara dni anteriores con el que se va a insertar y se cinomprueba si la fecha de inicio + duracion es mayor a la fecha actual
+
+    IF v_num_alquileres >= 3 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'El usuario ya tiene 3 alquileres activos.');
+    END IF;
+END;
+/
