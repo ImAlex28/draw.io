eeb754a964b5ac2a5f66756e5cc522d84b887594
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-04-10 16:42:25 +0200

    AlejandroGomez's Querys Finished

--- AlejandroGomez.sql
+++ AlejandroGomez.sql
@@ -1,9 +1,19 @@
 -- CONSULTAS
 -- 1. Mostrar todos los ejemplares nuevos en venta de un libro según su ISBN.
-SELECT l.ISBN, l.TITULO, e.ID as ID_EJEMPLAR, e.ESTADO, e.FORMATO, e.PRECIO_VENTA FROM LIBRO_TABl, TABKE(l.EJEMPLARESVENTA) e WHERE l.ISBN = '0123456789123' AND e.ESTADO = 'Nuevo';
+SELECT l.ISBN, 
+       l.TITULO, 
+       DEREF(VALUE(e)).ID AS ID_EJEMPLAR, 
+       DEREF(VALUE(e)).ESTADO AS ESTADO, 
+       DEREF(VALUE(e)).FORMATO AS FORMATO, 
+       DEREF(VALUE(e)).PRECIO_VENTA AS PRECIOVENTA
+FROM LIBRO_TAB l, 
+     TABLE(CAST(l.EJEMPLARESVENTA AS EJEMPLARESVENTA_NTABTYP)) e
+WHERE l.ISBN = '9783161484100'
+AND DEREF(VALUE(e)).ESTADO = 'Nuevo';
 
 -- 2. Media del precio de venta por estado del ejemplar.
-SELECT e.ESTADO, AVG(e.PRECIO_VENTA) as MEDIA_PRECIO FROM LIBRO_TAB l, TABLE(l.EJEMPLARESVENTA) e GROUP BY e.ESTADO;
+SELECT DEREF(VALUE(e)).ESTADO AS ESTADO, AVG(DEREF(VALUE(e)).PRECIO_VENTA) AS MEDIA_PRECIO
+FROM LIBRO_TAB l, TABLE(CAST(l.EJEMPLARESVENTA AS EJEMPLARESVENTA_NTABTYP)) e GROUP BY DEREF(VALUE(e)).ESTADO;
 
 -- Trigger
 -- Trigger que impide la inserción de un alquiler si el usuario ya tiene 3 alquileres activos.
@@ -19,5 +29,15 @@ BEGIN
     IF v_num_alquileres >= 3 THEN
         RAISE_APPLICATION_ERROR(-20001, 'El usuario ya tiene 3 alquileres activos.');
     END IF;
+END;
+/
+
+-- Prodedimiento
+-- Procedimiento que aumenta el precio de venta de todos los ejemplares de un libro en una cantidad dada.
+CREATE OR REPLACE PROCEDURE subir_precio_libro ( p_isbn IN CHAR, p_incremento IN NUMBER) IS
+BEGIN
+    FOR l IN ( SELECT REF(libro) AS ref_libro FROM LIBRO_TAB libro WHERE libro.ISBN = p_isbn)
+    LOOP UPDATE TABLE(SELECT libro.EJEMPLARESVENTA FROM LIBRO_TAB libro WHERE REF(libro) = l.ref_libro) SET PRECIOVENTA = PRECIOVENTA + p_incremento;
+  END LOOP;
 END;
 /
